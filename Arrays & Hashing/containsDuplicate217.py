4b2a6c7f613092d03f233106c067f5b348380043
shrechan <shrechan@iu.edu>
2025-02-01 20:20:17 -0500

    NeetCode150 Contains Duplicates I 217 Update

--- Arrays & Hashing/containsDuplicate217.py
+++ Arrays & Hashing/containsDuplicate217.py
@@ -7,3 +7,16 @@ class Solution:
             elif i not in container:
                 container.add(i)
         return False
+    
+
+# Time Complexity Analysis
+
+#If you use the two pointer method i.e two loops
+#then time complexity would be O(n2)
+
+#If you use the sorting method first and then compare the elements
+#then time complecity would O(nlogn)
+
+#If you use a HashSet as shown in the above code 
+#Time complexity will be O(n)
+#Space complexity O(n)
