a435f7e49af9feb8d2deb2879a859b0e759e518f
Shreyas Chandrashekar <44034499+ShreyasChandrashekar@users.noreply.github.com>
2025-02-01 20:10:13 -0500

    Delete containsDuplicate217.py

--- containsDuplicate217.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        container = set()
-        for i in nums:
-            if i in container:
-                return True
-            elif i not in container:
-                container.add(i)
-        return False
