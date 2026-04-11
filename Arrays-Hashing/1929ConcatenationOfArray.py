da76c190d7c97ee202557a1633365866af019090
Chandrashekar, Shreyas <shrechan@iu.edu>
2026-04-11 01:45:37 -0400

    1929 Concatenation of Array

--- Arrays-Hashing/1929ConcatenationOfArray.py
+++ Arrays-Hashing/1929ConcatenationOfArray.py
@@ -1,3 +1,12 @@
+# Algorithm: Concatenation of Array
+# Time Complexity: O(n)
+# Space Complexity: O(1) (in-place modification)
+
+# Use Slicing to concatenate the array by appending a copy of the original array to itself.
+# If m is the length of the original array, we can assign the elements from index m to m-1 on one side
+# to the elements from index 0 to m-1 on the other side, effectively doubling the array in place.
+
+from typing import List
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         m = len(nums)
